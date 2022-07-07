ea16a106bb1aad59c62978b84e960a8eb1c5d420
Mounisha.M <Mounisha.M@SEZLP657.avasoft.com>
2022-07-07 21:16:45 +0530

    Commit

--- ABC/src/test/java/Java/ABC/AppTest.java
+++ ABC/src/test/java/Java/ABC/AppTest.java
@@ -1,38 +1,52 @@
 package Java.ABC;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
+import java.io.File;
+
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+	
+
+	@Test
+	public void Test1() throws Exception{ 
+		
+		System.out.println("Hi");
+	}
+	
+	@Test
+	public void Test2() throws Exception{ 
+		
+		WebDriverManager.chromedriver().setup();
+        // Initialize browser
+        WebDriver driver = new ChromeDriver();
+        
+        //navigate to url
+        driver.get("https://demoqa.com");
+        
+       //Take the screenshot
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        
+        //Copy the file to a location and use try catch block to handle exception
+        try {
+            FileUtils.copyFile(screenshot, new File("D:\\Selenium Automation\\homePageScreenshot.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        //closing the webdriver
+        driver.close();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
