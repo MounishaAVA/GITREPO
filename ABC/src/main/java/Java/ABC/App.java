aaef292d2098d4fc7efc9437c69c1a807e832a62
Mounisha.M <Mounisha.M@SEZLP657.avasoft.com>
2022-07-12 19:05:54 +0530

    Commit

--- ABC/src/main/java/Java/ABC/App.java
+++ ABC/src/main/java/Java/ABC/App.java
@@ -45,6 +45,7 @@ public class App {
 		driver.quit();
 		
 
+		
 	}
 	
 	@Test
---
+++ ABC/src/main/java/Java/ABC/Excelreport.java
@@ -0,0 +1,12 @@
+package Java.ABC;
+
+import org.automationtesting.excelreport.Xl;
+
+public class Excelreport {
+
+	public static void main(String[] args) throws Exception {
+		
+		Xl.generateReport("Excelreport.xlsx");
+		
+	}
+}
